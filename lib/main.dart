a3aadf44e3e5ff982394faca1a81d20b08f44f36
kauanysc16 <123770473+kauanysc16@users.noreply.github.com>
2024-12-02 09:03:05 -0300

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
-import 'login_page.dart'; // Importe sua tela de login
+import 'package:firebase_core/firebase_core.dart';
+import 'package:pet_amigo/views/cadastro_page.dart';
+import 'package:pet_amigo/views/home_page.dart';
+import 'package:pet_amigo/views/login_page.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,9 +23,9 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => TelaPrincipal(),
+        '/': (context) => const TelaPrincipal(),
         '/login': (context) => const LoginPage(),
-        '/register': (context) => RegisterPage(), // Certifique-se de ter a tela de cadastro
+        '/register': (context) => const CadastroPage(),
       },
     );
   }
